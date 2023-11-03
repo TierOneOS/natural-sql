4dab28274a15fac24a22d803d8fb491462fecf3e
Mariano Ravinale <mravinale@gmail.com>
2023-11-03 16:39:48 -0300

    minor improvements

--- app/routers/queryRouter.py
+++ app/routers/queryRouter.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from ..services.queryService import QueryService
 
 router = APIRouter()
+query_service = QueryService()
+
 
 @router.get("/query")
 async def get_query():
-    return await QueryService.get_query()
+    return await query_service.get_query()
--- app/services/queryService.py
+++ app/services/queryService.py
@@ -2,5 +2,5 @@ from typing import List, Dict, Union
 
 
 class QueryService:
-    async def get_query() -> List[Dict[str, Union[int, str]]]:
+    async def get_query(self) -> List[Dict[str, Union[int, str]]]:
         return [{"item_id": 1, "q": "test"}]
